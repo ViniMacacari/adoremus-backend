31f5c1db98b1a3a305ff1f0b2bee58d3d030dbb2
ViniMacacari <macacarivinicius@gmail.com.br>
2024-10-23 13:06:23 -0300

    Orações do terço mariano

--- src/routes/santo-terco/buscar.ts
+++ src/routes/santo-terco/buscar.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+
 import TercoMariano from '../../controllers/santo-terco/tercos/terco-mariano'
+
+import OracoesTerco from '../../controllers/santo-terco/oracoes/oracoes-terco'
 
 class RotaBuscarSantoTerco {
     public router: Router
@@ -10,7 +13,8 @@ class RotaBuscarSantoTerco {
     }
 
     private registrarRotas(): void {
-        this.router.get('/buscar/terco-mariano', (req, res) => TercoMariano.informacoes(req, res))
+        this.router.get('/buscar/terco/mariano', (req, res) => TercoMariano.informacoes(req, res))
+        this.router.get('/buscar/oracoes/terco', (req, res) => OracoesTerco.informacoes(req, res))
     }
 }
 
