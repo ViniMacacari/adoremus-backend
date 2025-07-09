948f3a98f0409a629fbf0a9c578f0dbd5a3a9cae
ViniMacacari <macacarivinicius@gmail.com>
2025-07-09 11:13:42 -0300

    feat: separação por grupos de mandamento

--- src/services/doutrine/commandments.ts
+++ src/services/doutrine/commandments.ts
@@ -7,11 +7,22 @@ export class CommandmentsService {
         try {
             const commandments = await this.db.exec('select * from view_mandamentos')
 
-            return {
-                mandamentos: commandments
+            const result = {
+                mandamentos10: [] as any[],
+                mandamentosIgreja: [] as any[]
             }
+
+            for (const cmd of commandments) {
+                if (cmd.grupo_mandamento === '10 Mandamentos') {
+                    result.mandamentos10.push(cmd)
+                } else if (cmd.grupo_mandamento === '5 Mandamentos da Igreja') {
+                    result.mandamentosIgreja.push(cmd)
+                }
+            }
+
+            return result
         } catch (error: any) {
-            throw new Error(`Erro ao obter os mistérios do terço do amor: ${error.message}`)
+            throw new Error(`Erro ao obter os mandamentos: ${error.message}`)
         }
     }
 }
