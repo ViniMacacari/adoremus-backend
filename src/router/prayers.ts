006f596f66a9b3c77ebb13444e0c18544ab90eca
ViniMacacari <macacarivinicius@gmail.com>
2025-07-09 10:10:57 -0300

    feat: endpoint de oração do terço da misericórdia

--- src/router/prayers.ts
+++ src/router/prayers.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { LoveChapletController } from '../controllers/prayers/love-chaplet.js'
+import { DivineMercyChapletController } from '../controllers/prayers/divine-mercy-chaplet.js'
 
 export class PrayersRouter {
     public readonly router: Router
 
     private loveChaplet: LoveChapletController = new LoveChapletController()
+    private divineMercyChaplet: DivineMercyChapletController = new DivineMercyChapletController()
 
     constructor() {
         this.router = Router()
@@ -13,5 +15,6 @@ export class PrayersRouter {
 
     private setupRoutes(): void {
         this.router.get('/terco-amor', this.loveChaplet.get.bind(this.loveChaplet))
+        this.router.get('/terco-misericordia', this.divineMercyChaplet.get.bind(this.divineMercyChaplet))
     }
 }
